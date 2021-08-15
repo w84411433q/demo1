e67e597d410e4bcb19a0d58e94878a6cf88bb5b3
wq84411433 <18215698288@163.com>
2021-08-15 23:38:07 +0800

    第二次修改

--- src/com/demo/Demo2.java
+++ src/com/demo/Demo2.java
@@ -3,5 +3,8 @@ package com.demo;
 public class Demo2 {
     public static void main(String[] args) {
         System.out.println("1111");
+        int a = 1;
+        int b = 2;
+        System.out.println(a + b);
     }
 }
